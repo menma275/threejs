a5012efc6074cdae5a472a1b151532d32589fdf7
sakamura <k.sakamura@sakamura-Mac.local>
2023-06-05 09:05:30 +0900

    最終

--- 02/sketch.js
+++ 02/sketch.js
@@ -3,14 +3,12 @@ window.addEventListener("resize", onResize);
 
 var renderer, camera;
 var colors, palette;
+var windowSize = 2000;
 
 function onResize() {
   // サイズを取得
-  var windowSize = Math.min(innerWidth, innerHeight);
-  // windowSize = 1000;
+  // windowSize = Math.min(innerWidth, innerHeight);
   var width = windowSize / Math.sqrt(2);
-  // width = windowSize;
-  // width = (windowSize * 3) / 4;
   var height = windowSize;
 
   // レンダラーのサイズを調整する
@@ -22,32 +20,12 @@ function onResize() {
   camera.updateProjectionMatrix();
 }
 
-function interpolate(n) {
-  var startN = 5;
-  var startProb = 0.4;
-  var endN = 10;
-  var endProb = 0.2;
-
-  if (n <= startN) {
-    return startProb;
-  } else if (n >= endN) {
-    return endProb;
-  } else {
-    var t = (n - startN) / (endN - startN);
-    var prob = startProb * (1 - t) + endProb * t;
-    return prob;
-  }
-}
-
 function init() {
   // 画面サイズ
-  var windowSize = Math.min(innerWidth, innerHeight);
+  // windowSize = Math.min(innerWidth, innerHeight);
   var width = windowSize / Math.sqrt(2);
-  // width = windowSize;
-  // width = (windowSize * 3) / 4;
   var height = windowSize;
 
-  // colors = colorPalette();
   colors = generativePalette();
 
   renderer = new THREE.WebGLRenderer({
@@ -60,50 +38,36 @@ function init() {
   const scene = new THREE.Scene();
   scene.background = new THREE.Color(colors[0]);
 
-  // camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-
   camera = new THREE.OrthographicCamera(
     -width / 2,
     width / 2,
     height / 2,
     -height / 2,
-    0.1,
-    10000
+    0,
+    windowSize * 1.5
   );
-  camera.position.set(0, 500, 1000);
-  // camera.position.set(0, 500, 1000);
+  camera.position.set(0, height / 2, height);
   camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-  const prob = interpolate(n);
   const offset = width / 2.75;
   const offsetY = (height - offset) / 2;
 
   // ヘルパーを出す
-  const axesHelper = new THREE.AxesHelper(1000);
+  const axesHelper = new THREE.AxesHelper(windowSize);
   // scene.add(axesHelper);
 
-  var brightColor = colors[1];
-  var darkColor = colors[2];
+  var color1 = colors[1];
+  var color2 = colors[2];
 
-  var bHSL = {};
-  var dHSL = {};
-  brightColor.getHSL(bHSL);
-  darkColor.getHSL(dHSL);
-
-  if (bHSL.l < dHSL.l) {
-    brightColor = new THREE.Color(colors[2]);
-    darkColor = new THREE.Color(colors[1]);
-  }
-
-  // shaderのマテリアル
   const uniforms = {
     windowSize: { type: "f", value: windowSize },
-    color1: { type: "vec3", value: darkColor },
-    color2: { type: "vec3", value: brightColor },
+    dimension: { type: "f", value: Math.random() < 0.5 ? 1.0 : -1.0 },
+    color1: { type: "vec3", value: color1 },
+    color2: { type: "vec3", value: color2 },
   };
 
   const vertexShader = `
-    precision mediump float;
+    precision highp float;
     varying vec2 vUv;
     void main() {
       gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
@@ -111,8 +75,8 @@ function init() {
     }
   `;
 
-  const fragmentShader = `
-    precision mediump float;
+  const boxFragmentShader = `
+    precision highp float;
     varying vec2 vUv;
     uniform vec3 color1;
     uniform vec3 color2;
@@ -158,38 +122,62 @@ function init() {
       vec2 fuv = -1.0 + 2.0 * vUv;
 
       float c = sin(fuv.s*fuv.t);
-      // c = snoise(fuv*.2);
-
-      // c += rand(fuv)*0.25;
       c += rand(fuv)*0.0005*windowSize;
 
       vec3 color = mix(color1, color2, c);
       // color = vec3(c);
+      gl_FragColor = vec4(color, 1.0);
+    }
+  `;
+
+  const sphereFragmentShader = `
+    precision highp float;
+    varying vec2 vUv;
+    uniform vec3 color1;
+    uniform vec3 color2;
+    uniform float windowSize;
+
+    float rand(vec2 n) { 
+      return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
+    }
+
+    void main() {
+
+      vec2 fuv = -1.0 + 2.0 * vUv;
+
+      float c = fuv.t;
+      c += rand(fuv)*0.0005*windowSize;
+
+      vec3 color = mix(color1, color2, c);
 
       gl_FragColor = vec4(color, 1.0);
     }
   `;
 
-  console.log(windowSize);
-
   const blockNumMax = 3;
   for (let blockNum = 0; blockNum < blockNumMax; blockNum++) {
     var n = Math.floor(Math.random() * 2) + 2;
     var boxSize = (width - offset * 2) / n;
-    const boxGeometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize);
-    const spehereGeometry = new THREE.SphereGeometry(boxSize / 2, 32, 16);
-    var material = new THREE.MeshBasicMaterial({
-      color: colors[1],
-    });
 
-    material = new THREE.ShaderMaterial({
+    const boxGeometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize);
+    const spehereGeometry = new THREE.SphereGeometry(boxSize / 2, 64, 32);
+
+    var boxMaterial = new THREE.ShaderMaterial({
       uniforms: uniforms,
       vertexShader: vertexShader,
-      fragmentShader: fragmentShader,
+      fragmentShader: boxFragmentShader,
       transparent: true,
     });
-    var box = new THREE.Mesh(boxGeometry, material);
-    var sphere = new THREE.Mesh(spehereGeometry, material);
+
+    var sphereMaterial = new THREE.ShaderMaterial({
+      uniforms: uniforms,
+      vertexShader: vertexShader,
+      fragmentShader: sphereFragmentShader,
+      transparent: true,
+    });
+
+    var box = new THREE.Mesh(boxGeometry, boxMaterial);
+    var sphere = new THREE.Mesh(spehereGeometry, sphereMaterial);
     var coordinates = [];
     for (let x = 0; x < n; x++) {
       for (let y = 0; y < n; y++) {
@@ -208,7 +196,7 @@ function init() {
           var p = i + j * n * n + k * n;
           if (!coordinates[p]) {
             var object = box.clone();
-            var cloneMaterial = material.clone();
+            var material = boxMaterial.clone();
 
             var setTrueMax;
             var searchStep;
@@ -261,36 +249,32 @@ function init() {
               Math.random() < 0.5
             ) {
               object = sphere.clone();
-              object.rotation.y = 180 * (Math.PI / 180);
+              material = sphereMaterial.clone();
+              object.rotation.x = Math.random() * 360 * (Math.PI / 180);
+              object.rotation.z = Math.random() * 360 * (Math.PI / 180);
+              object.rotation.y = Math.random() * 360 * (Math.PI / 180);
             }
 
-            var offsetSize = 0.3;
-            object.scale.set(
-              boxSizeX - offsetSize,
-              boxSizeY - offsetSize,
-              boxSizeZ - offsetSize
-            );
-
-            var colorNum = Math.floor(Math.random() * (colors.length - 1)) + 1;
             var colorNum0 = Math.floor(Math.random() * (colors.length - 1)) + 1;
             var colorNum1 = Math.floor(Math.random() * (colors.length - 1)) + 1;
             while (colorNum0 == colorNum1) {
               colorNum1 = Math.floor(Math.random() * (colors.length - 1)) + 1;
             }
 
-            cloneMaterial.uniforms.color1.value = new THREE.Color(
-              colors[colorNum0]
-            );
-            cloneMaterial.uniforms.color2.value = new THREE.Color(
-              colors[colorNum1]
-            );
-            // cloneMaterial.color = new THREE.Color(colors[colorNum]);
-            object.material = cloneMaterial;
+            material.uniforms.color1.value = new THREE.Color(colors[colorNum0]);
+            material.uniforms.color2.value = new THREE.Color(colors[colorNum1]);
+            object.material = material;
 
+            // タイリングのポジション
+            var offsetSize = 0.3;
+            object.scale.set(
+              boxSizeX - offsetSize,
+              boxSizeY - offsetSize,
+              boxSizeZ - offsetSize
+            );
             var adjustX = (boxSizeX / 2) * boxSize;
             var adjustY = (boxSizeY / 2) * boxSize;
             var adjustZ = (boxSizeZ / 2) * boxSize;
-            console.log("blockNum : " + blockNum);
             object.position.x = -width / 2 + offset + i * boxSize + adjustX;
             object.position.y =
               (-height / blockNumMax) * Math.sqrt(3) +
@@ -300,27 +284,19 @@ function init() {
               j * boxSize +
               adjustY;
             object.position.z = -width / 2 + offset + k * boxSize + adjustZ;
+
             scene.add(object);
           }
         }
       }
     }
   }
-  // ビューを変化
-  // const rotateXZ = Math.random();
-  // if (rotateXZ > 1 - 1 / 3) scene.rotation.z = 90 * (Math.PI / 180);
-  // else if (rotateXZ < 1 / 3) scene.rotation.x = 15 * (Math.PI / 180);
-  // if (Math.random() < 0.5) scene.rotation.y = 45 * (Math.PI / 180);
-  // else scene.rotation.y = -45 * (Math.PI / 180);
-
   scene.rotation.x = 15 * (Math.PI / 180);
   scene.rotation.y = 45 * (Math.PI / 180);
 
   tick();
 
   function tick() {
-    // scene.rotation.x += 0.005;
-    // scene.rotation.y += 0.005;
     requestAnimationFrame(tick);
     renderer.render(scene, camera);
   }
